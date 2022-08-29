e8e21567439bec0c8ffbb031347602405eab8b82
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 20:09:51 +0530

    Add files via upload

---
+++ qs.js
@@ -0,0 +1,50 @@
+// // varx=5,y=1; 
+// // var obj ={ x:10} ; 
+// // with (obj) ; 
+// // {  
+// // console.log(y)
+// // }
+
+
+// for(var number=10;number>=1;number--)
+// {  
+// console.log(number)  
+// }  
+// var number=10;  
+// while(number>=1)  
+// {  
+// console.log(number) 
+//        number--;  
+// }  
+
+// var count =0;  
+// while (count <10)  
+// {  
+//      console.log(count);  
+//      count++;  
+// }  
+
+
+// varx=5,y=1  
+// var obj ={ x:10}  
+// with(obj)  
+// {  
+//       console.log(y)  
+// }  
+
+
+// var a=5;
+// var a=8;
+// console.log(a)
+
+/* <p id="demo"></p>var txt1 ="Sanfoundry_";var txt2 = "Javascriptmcq";document.getElementById("demo").innerHTML = txt1 + txt2; */
+// number
+// 
+
+// var information = 'I am staying in the "Sarjapur Campus"'
+// console.log(information)
+// // output: I am staying in the "Sarjapur Campus"
+
+var information = "I am staying in the \"Sarjapur Campus\""
+console.log(information)
+// output: I am staying in the "Sarjapur Campus"
