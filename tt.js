bbb52008134e7ba98c9f09287c958326559d925d
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 20:07:34 +0530

    Add files via upload

---
+++ tt.js
@@ -0,0 +1,15 @@
+for (var a=1;a<=10;a++){
+    if (a%2==0){
+        // document.write(a+"br")
+        console.log("yes it is even",a)
+    }
+    else{
+        // document.write(a+"br")
+
+        console.log("it is odd no",a)
+    }
+}
+
+
+var ename = "shivani";
+console.log(ename)
